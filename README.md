353c50f130ae8501a367f3e649ff1b7c48c27f14
Lucas Sernaglia <lucas.sernaglia@gmail.com>
2020-10-26 10:33:04 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# tic-tac-toe-flutter
+
+Starting a project in flutter!
+This application was developed by Professor Kleber, 
+from Fatec Americana, and will be used to assess Flutter skills.
+
---
+++ lib/core/constants.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+const int BOARD_SIZE = 9;
+
+const String PLAYER1_SYMBOL = 'robin';
+const Color PLAYER1_COLOR = Colors.pink; // Alfa RGB (0xFFFF0000)
+const String PLAYER1_IMG = 'assets/robin.png';
+
+const String PLAYER2_SYMBOL = 'batman';
+const Color PLAYER2_COLOR = Colors.black;
+const String PLAYER2_IMG = 'assets/batma.jpg';
+
+const String GAME_TITLE = 'Tic Tac Toe';
+const String TIED_TITLE = 'Game Tied!';
+const String WIN_TITLE = 'Player [SYMBOL] won!';
+const String DIALOG_MESSAGE = 'Press the reset button to start again!';
+const String SINGLE_PLAYER_MODE = 'Single Player';
+const String MULTIPLAYER_MODE = 'Two Players';
+const String RESET_BUTTON_LABEL = 'RESET';
